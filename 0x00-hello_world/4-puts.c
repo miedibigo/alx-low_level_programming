6c330ba12f3ce57d4559e7e4c84946b88e77e1f0
miedibigo <miedibi90@gmail.com>
2022-06-30 19:35:29 -0700

    tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,7 @@
 #!/bin/bash
-#include <stdio.h> 
-int main(void) { printf("\"Programming is like building a multilingual puzzle\n"); return (0); }
+#include <stdio.h>
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
